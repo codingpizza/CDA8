fe5e4b659427e0014a352695d67560263564cb7d
Giuseppe Vetri <gvetri18@gmail.com>
2018-05-27 16:54:54 +0200

    First Commit

---
+++ app/src/main/java/com/codingpizza/cda8/Pizza.java
@@ -0,0 +1,43 @@
+package com.codingpizza.cda8;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Pizza {
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("description")
+    @Expose
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
